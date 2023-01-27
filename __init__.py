528f54dd7babed8a394ed5ec142afdce10a15639
Jean-Luc Tibaux <eUgEntOptIc@outlook.de>
2023-01-27 12:57:20 +0000

    improved code readability

--- __init__.py
+++ __init__.py
@@ -5,14 +5,17 @@ import sys
 
 import requests
 
-from wetterdienst import Wetterdienst, Resolution, Period
+from wetterdienst import Wetterdienst # , Resolution, Period
 from wetterdienst.provider.dwd.mosmix import DwdMosmixType
 
-twfg_req = requests.get('https://tinyweatherforecastgermanygroup.gitlab.io/index/twfg-stations.json', timeout=10)
+twfg_req = requests.get('https://tinyweatherforecastgermanygroup.gitlab.io/index/'
+                        'twfg-stations.json',
+                        timeout=10)
 if twfg_req.ok:
     twfg_req = twfg_req.json()
 else:
-    print(f"ERROR: failed to download 'twfg-stations.json' -> server returned unexpected status code '{twfg_req.status_code}' ")
+    print("ERROR: failed to download 'twfg-stations.json' -> server returned"
+          f" unexpected status code '{twfg_req.status_code}' ")
     sys.exit(1)
 
 with open('twfg-stations.json', 'w+', encoding='utf-8') as fh:
@@ -35,7 +38,8 @@ station_names = list(stations_df['name'])
 for row_index, stations_row in stations_df.iterrows():
     station_to_dt = str(stations_row['to_date'])
     if station_to_dt != 'NaT': #TODO: double check this with wetterdienst docs
-        print(f"WARNING: station '{stations_row['name']}' ({stations_row['station_id']}) -> to_date: {station_to_dt} ")
+        print(f"WARNING: station '{stations_row['name']}'"
+              f" ({stations_row['station_id']}) -> to_date: {station_to_dt} ")
 
     stations_list.append({
         'id':stations_row['station_id'],
@@ -45,22 +49,20 @@ for row_index, stations_row in stations_df.iterrows():
         'altitude':stations_row['height']
     })
 
-    # if stations_row['name'] not in station_names_old:
-    #     print(stations_row['name'])
-
 print(f"DEBUG: retrieved {len(stations_list)} MOSMIX stations via wetterdienst")
 with open('stations.json', 'w+', encoding='utf-8') as fh:
     fh.write(str(json.dumps(stations_list, indent=4)))
 
-print(f"DEBUG: new station(s):")
+print("DEBUG: new station(s):")
 new_stations_names = list(set(station_names) - set(station_names_old))
 pprint(new_stations_names)
 
-print(f"DEBUG: deprecated or removed station(s):")
+print("DEBUG: deprecated or removed station(s):")
 old_stations_names = list(set(station_names_old) - set(station_names))
 pprint(old_stations_names)
 
 with open('stations-report.json', 'w+', encoding='utf-8') as fh:
-    fh.write(str(json.dumps({'new':new_stations_names,'old':old_stations_names}, indent=4)))
+    fh.write(str(json.dumps({'new':new_stations_names,'old':old_stations_names},
+                            indent=4)))
 
 print("done")
